2ae9f6fa4c94f014ee405c86a1822a9c1b007418
novichenkoAlexander <a.novichenko1996@gmail.com>
2021-02-04 11:41:09 +0300

    Add some major changes!

--- Lesson4.1/src/com/home/model/Computer.java
+++ Lesson4.1/src/com/home/model/Computer.java
@@ -10,6 +10,13 @@ public class Computer {
     private String ram;
     private String hardDisk;
     private int lifeCycle;
+    private int status;     //0 - isDead, 1 - On, 2 - Off
+    private int cycleCounter;
+
+    {
+        status = 2;
+        cycleCounter = 0;
+    }
 
     public Computer(String cpu, String ram, String hardDisk, int lifeCycle) {
         this.cpu = cpu;
@@ -50,7 +57,6 @@ public class Computer {
             System.out.println("PC has burned down! Out of life cycles! ");
             setStatus(0);
         }
-        return state;
 
     }
 
@@ -59,6 +65,8 @@ public class Computer {
             System.out.println("PC is turning off...");
             if (getRandomNumber()) {
                 System.out.println("PC is OFF!");
+                setStatus(2);       // PC is Off
+                setCycleCounter(+1);
             } else {
                 System.out.println("PC has burned down!!!");
                 setStatus(0);
@@ -73,6 +81,22 @@ public class Computer {
         System.out.println("ALARM! Input 0 or 1");
         int check = Reader.readInt();
         return oneZero == check;
+    }
+
+    public int getCycleCounter() {
+        return cycleCounter;
+    }
+
+    public void setCycleCounter(int cycleCounter) {
+        this.cycleCounter = cycleCounter;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
     }
 
     public String getCpu() {
