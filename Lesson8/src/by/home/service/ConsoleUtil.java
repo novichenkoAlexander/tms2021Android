b642287fe9ad6405782b6639c452ccfd9cd0d63e
novichenkoAlexander <a.novichenko1996@gmail.com>
2021-02-24 20:59:09 +0300

    Remade task of writing string input in file

--- Lesson8/src/by/home/service/ConsoleUtil.java
+++ Lesson8/src/by/home/service/ConsoleUtil.java
@@ -1,20 +1,24 @@
 package by.home.service;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class ConsoleUtil {
     private static Scanner scanner;
 
-    public static String readString() {
-        System.out.println("Input string: ");
-        String string = "";
-        while (true) {
-            scanner = new Scanner(System.in);
-            if (scanner.hasNextLine()) {
-                string = scanner.nextLine();
-                break;
-            }
+    public static void writeInputStringInFile(String outputFile) throws IOException {
+        try (BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile))) {
+            String inputString = bf.readLine();
+            bw.write(reverseString(inputString));
+            bw.flush();
+            System.out.println("File has been overwritten!");
         }
-        return string;
     }
+
+    private static String reverseString(String input) {
+        StringBuilder builder = new StringBuilder(input);
+        return builder.reverse().toString();
+    }
+
 }
