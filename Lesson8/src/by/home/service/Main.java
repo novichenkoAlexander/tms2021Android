93eb3743db878fd6079b35c07619ba517129abdb
novichenkoAlexander <a.novichenko1996@gmail.com>
2021-02-25 00:05:33 +0300

    Minor changes added

--- Lesson8/src/by/home/service/Main.java
+++ Lesson8/src/by/home/service/Main.java
@@ -1,24 +1,52 @@
 package by.home.service;
 
-import java.io.*;
+import by.home.input.HomeFileReader;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 
 public class Main {
     private static final String RESOURCE_DIR = "Lesson8/resources/";
-    private static final String OUTPUT_FILE = RESOURCE_DIR + "tmp.txt";
+    private static final String NEW_TEXT_FILE = RESOURCE_DIR + "newText.txt";
+    private static final String OUTPUT_FILE = RESOURCE_DIR + "output.txt";
 
+    static {
+        new File(NEW_TEXT_FILE).delete();
+        new File(OUTPUT_FILE).delete();
+    }
 
     public static void main(String[] args) throws IOException {
-        String inputString = ConsoleUtil.readString();
-        StringBuilder builder = new StringBuilder(inputString);
-        String reverseString = builder.reverse().toString();
 
-        try (FileOutputStream fos = new FileOutputStream(OUTPUT_FILE)) {
-            fos.write(reverseString.getBytes());
-            System.out.println("File has been overwritten!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+        /**
+         * Writing reversed string in file output.txt
+         */
+//        Util.writeInputStringInFile(OUTPUT_FILE);
+
+        /**
+         * Task № 2: Read from text.txt. Write in newText.txt sentences that contain from 3 to 5 words.
+         * If there is palindrome in sentence - write this sentence in new file no matter how many words in it.
+         */
+        String filePath = RESOURCE_DIR + Util.readStringFromConsole("File name");
+        System.out.println(filePath);
+
+        //Reading file
+        HomeFileReader reader = new HomeFileReader(filePath);
+        String textFromFile = reader.readAllFile();
+        System.out.println(textFromFile);
+
+        //Divide text by sentences
+        SentenceSeparator separator = new SentenceSeparator(textFromFile);
+        List<String> sentences = separator.divideTextBySentences();
+        System.out.println(sentences.size());
+
+        /**
+         * Writing appropriate sentences in file newText.txt
+         */
+        Util.addAppropriateTextInFile(sentences, NEW_TEXT_FILE);
+
     }
 }
 
