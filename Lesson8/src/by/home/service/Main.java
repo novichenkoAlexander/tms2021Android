63cf9fda3da8e12977d8ec9d9fdfd06553fcb8cd
novichenkoAlexander <a.novichenko1996@gmail.com>
2021-02-27 15:48:22 +0300

    Additional hw part 1 done

--- Lesson8/src/by/home/service/Main.java
+++ Lesson8/src/by/home/service/Main.java
@@ -1,14 +1,14 @@
 package by.home.service;
 
-import by.home.input.HomeFileReader;
-
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 
 public class Main {
     private static final String RESOURCE_DIR = "Lesson8/resources/";
+    private static final String DIR_NAME = "dir";
+    private static final String NEW_DIR_NAME = "newDir";
+    private static final String ZIP_NAME = "dir.zip";
     private static final String NEW_TEXT_FILE = RESOURCE_DIR + "newText.txt";
     private static final String OUTPUT_FILE = RESOURCE_DIR + "output.txt";
 
@@ -29,23 +29,35 @@ public class Main {
          * Task № 2: Read from text.txt. Write in newText.txt sentences that contain from 3 to 5 words.
          * If there is palindrome in sentence - write this sentence in new file no matter how many words in it.
          */
-        String filePath = RESOURCE_DIR + Util.readStringFromConsole("File name");
-        System.out.println(filePath);
-
-        //Reading file
-        HomeFileReader reader = new HomeFileReader(filePath);
-        String textFromFile = reader.readAllFile();
-        System.out.println(textFromFile);
-
-        //Divide text by sentences
-        SentenceSeparator separator = new SentenceSeparator(textFromFile);
-        List<String> sentences = separator.divideTextBySentences();
-        System.out.println(sentences.size());
+//        String filePath = RESOURCE_DIR + Util.readStringFromConsole("File name");
+//        System.out.println(filePath);
+//
+//        //Reading file
+//        HomeFileReader reader = new HomeFileReader(filePath);
+//        String textFromFile = reader.readAllFile();
+//        System.out.println(textFromFile);
+//
+//        //Divide text by sentences
+//        SentenceSeparator separator = new SentenceSeparator(textFromFile);
+//        List<String> sentences = separator.divideTextBySentences();
+//        System.out.println(sentences.size());
 
         /**
          * Writing appropriate sentences in file newText.txt
          */
-        Util.addAppropriateTextInFile(sentences, NEW_TEXT_FILE);
+//        Util.addAppropriateTextInFile(sentences, NEW_TEXT_FILE);
+
+        /**
+         * ---Additional task---:
+         * Create directory on HDisk. (+)
+         * In the directory crete 3 files with info. (+)
+         * Archive this 3 files in one archive.(+)
+         * Rename directory.(+)
+         * Print in console all includes.(+)
+         * Delete directory.(+)
+         */
+        FileUtil fileUtil = new FileUtil(RESOURCE_DIR, DIR_NAME, ZIP_NAME, NEW_DIR_NAME);
+        fileUtil.workWithFiles();
 
     }
 }
